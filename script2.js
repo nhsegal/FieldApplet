6c086b858935c49e02d2c4699e5757db7bc5b5ff
Noah Segal <n.h.segal@gmail.com>
2015-03-26 19:21:11 -0400

    decreased steps in fieldlines
    This should speed up the field lines a little

--- script2.js
+++ script2.js
@@ -267,7 +267,7 @@ function fieldline(x, y) {
   var breakerBack = false; 
 
 //extend the list of tests in either direction
-  for (var i = 0; i <600; i++) {
+  for (var i = 0; i <140; i++) {
     if (breakerFront == false) {
       tests.push(new testCharge(x, y));
       tests[tests.length-1].updateEtot();
